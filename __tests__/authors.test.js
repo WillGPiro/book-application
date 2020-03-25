08958cac9df40e3312fe1f99c7e25b34d4b83123
Will G Piro <willgpiro@gmail.com>
2020-03-25 15:58:55 -0700

    it gets a book by id test and route

--- __tests__/authors.test.js
+++ __tests__/authors.test.js
@@ -1,3 +1,41 @@
+const { getBook, getAuthor } = require('../db/data-helpers');
+
 const request = require('supertest');
 const app = require('../lib/app');
+
+
+describe('author routes', () => {
+  it('creates a author', async() => {
+    return request(app)
+      .post('/api/v1/authors')
+      .send({
+        name: 'Palichuck'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          name: 'Palichuck',
+          __v: 0
+        });
+      });
+  });
+
+  it('gets an author by id', async() => {
+    const author = await getAuthor();
+    const books = await getBook({ authorId: author._id });
+
+    return request(app)
+      .get(`/api/v1/authors/${author._id}`)
+      .then(res => {
+        expect(res.body).toEqual({
+          ...author,
+          books
+        });
+      }); 
+  });
+
+});
+
+
+
 
--- __tests__/books.test.js
+++ __tests__/books.test.js
@@ -1,27 +1,39 @@
-const { getComment, getTweet } = require('../db/data-helpers');
+const { getBook, getAuthor } = require('../db/data-helpers');
 
 const request = require('supertest');
 const app = require('../lib/app');
 
-
-describe(' author routes', () => {
-  it('creates a author', async() => {
+describe('book routes', () => {
+  it('creates a book', async() => {
+    const author = await getAuthor();
     return request(app)
-      .post('/api/v1/authors')
+      .post('/api/v1/books')
       .send({
-        name: 'Palichuck'
+        authorId: author._id,
+        title: 'A Union of Geniuses',
+        pages: 98
       })
       .then(res => {
         expect(res.body).toEqual({
           _id: expect.any(String),
-          name: 'Palichuck',
+          authorId: author._id,
+          title: 'A Union of Geniuses',
+          pages: 98,
           __v: 0
         });
       });
   });
+  it('gets a book by id', async() => {
+    const author = await getAuthor();
+    const book = await getBook({ authorId: author._id });
 
+    return request(app)
+      .get(`/api/v1/books/${book._id}`)
+      .then(res => {
+        expect(res.body).toEqual({
+          ...book,
+          authorId: author
+        });
+      });
+  });
 });
-
-
-
-
